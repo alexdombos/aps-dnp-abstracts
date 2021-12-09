6b0bc5ab6ff8e6d8aabf116f07df33520000e7ee
Alex Dombos <alex.dombos@gmail.com>
2021-12-09 15:06:10 -0500

    Consolidate session scraping

--- aps_dnp_trends.py
+++ aps_dnp_trends.py
@@ -5,7 +5,19 @@ from datetime import datetime
 from urllib.parse import urljoin
 import requests
 
-def scrape(abstract):
+def scrape_session(session):
+    try:
+        dates = [date.text.split(',', 1)[1].strip() for date in session.find_all('a', href = False) if date.text and date.text != 'Not Participating']
+        assert all(date == dates[0] for date in dates)
+        date_text = dates[0]
+    except IndexError:
+        date_text = session.find_all('font', attrs = {'size': -1})[0].text.split('\n')[1].split(',', 1)[1].strip()
+
+    date = datetime.strptime(date_text, "%B %d, %Y").strftime("%m/%d/%Y")
+    [session] = [heading.text for heading in session.find_all('h3') if all(part in heading.text for part in ('Session', ':'))]
+    return date, session
+
+def scrape_abstract(abstract):
     date = abstract.find(name = 'meta', attrs = {'name': 'citation_date'})['content']
     [identifier] = [heading.text.split(':')[1].strip() for heading in abstract.find_all(name = 'h3') if 'Abstract' in heading.text]
     [session] = [heading.text for heading in abstract.find_all(name = 'h3') if 'Session' in heading.text]
@@ -30,6 +42,8 @@ def main():
         session_response = requests.get(url = session_url)
         session_soup = BeautifulSoup(markup = session_response.content, features = 'html.parser')
 
+        session_data = scrape_session(session_soup)
+
         abstract_links = [link for link in session_soup.find_all(name = 'a', href = True)
                           if '/Session/' in link['href'] and 'showAbstract' not in link['href']]
         for abstract_link in abstract_links:
@@ -37,19 +51,11 @@ def main():
             abstract_response = requests.get(url = abstract_url)
             abstract_soup = BeautifulSoup(markup = abstract_response.content, features = 'html.parser')
 
-            try:
-                dates = [date.text.split(',', 1)[1].strip() for date in session_soup.find_all('a', href = False) if date.text and date.text != 'Not Participating']
-                assert all(date == dates[0] for date in dates)
-                date_text = dates[0]
-            except IndexError:
-                date_text = session_soup.find_all('font', attrs = {'size': -1})[0].text.split('\n')[1].split(',', 1)[1].strip()
-            date = datetime.strptime(date_text, "%B %d, %Y").strftime("%m/%d/%Y")
             identifier = abstract_link.text.split(':')[0].strip()
-            [session] = [heading.text for heading in session_soup.find_all('h3') if all(part in heading.text for part in ('Session', ':'))]
-            data = scrape(abstract_soup)
-            assert data[0] == date
-            assert data[1] == identifier
-            assert data[2] == session
+            abstract_data = scrape_abstract(abstract_soup)
+            assert abstract_data[0] == session_data[0]
+            assert abstract_data[1] == identifier
+            assert abstract_data[2] == session_data[1]
 
 if __name__ == '__main__':
     main()
