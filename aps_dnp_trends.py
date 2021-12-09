f4ef25056ed19a3738b00c08070c425aa5146543
Alex Dombos <alex.dombos@gmail.com>
2021-12-09 17:15:57 -0500

    Scrape data from all years

--- aps_dnp_trends.py
+++ aps_dnp_trends.py
@@ -8,6 +8,25 @@ import requests
 
 Abstract = namedtuple('Abstract', ['date', 'identifier', 'session', 'title', 'authors', 'text'])
 
+def all_aps_dnp_years():
+    return {2005: 'HAW05',
+            2006: 'DNP06',
+            2007: 'DNP07',
+            2008: 'DNP08',
+            2009: 'HAW09',
+            2010: 'DNP10',
+            2011: 'DNP11',
+            2012: 'DNP12',
+            2013: 'DNP13',
+            2014: 'HAW14',
+            2015: 'DNP15',
+            2016: 'DNP16',
+            2017: 'DNP17',
+            2018: 'HAW18',
+            2019: 'DNP19',
+            2020: 'DNP20',
+            2021: 'DNP21'}
+
 def scrape_session(session):
     try:
         dates = [date.text.split(',', 1)[1].strip() for date in session.find_all('a', href = False) if date.text and date.text != 'Not Participating']
@@ -36,31 +55,33 @@ def scrape_abstract(abstract):
 
 def main():
 
-    epitome_url = 'https://meetings.aps.org/Meeting/DNP19/APS_epitome'
-    epitome_response = requests.get(url = epitome_url)
-    epitome_soup = BeautifulSoup(markup = epitome_response.content, features = 'html.parser')
+    aps_dnp_years = all_aps_dnp_years()
+    for year, year_url_segment in aps_dnp_years.items():
+        epitome_url = f'https://meetings.aps.org/Meeting/{year_url_segment}/APS_epitome'
+        epitome_response = requests.get(url = epitome_url)
+        epitome_soup = BeautifulSoup(markup = epitome_response.content, features = 'html.parser')
 
-    session_links = sorted(set([link for link in epitome_soup.find_all(name = 'a', href = True)
+        session_links = sorted(set([link for link in epitome_soup.find_all(name = 'a', href = True)
                                 if 'Session' in link['href']]), key = lambda tag: tag['href'])
-    for session_link in session_links:
-        session_url = urljoin(epitome_url, session_link['href'])
-        session_response = requests.get(url = session_url)
-        session_soup = BeautifulSoup(markup = session_response.content, features = 'html.parser')
+        for session_link in session_links:
+            session_url = urljoin(epitome_url, session_link['href'])
+            session_response = requests.get(url = session_url)
+            session_soup = BeautifulSoup(markup = session_response.content, features = 'html.parser')
 
-        session_data = scrape_session(session_soup)
+            session_data = scrape_session(session_soup)
 
-        abstract_links = [link for link in session_soup.find_all(name = 'a', href = True)
-                          if '/Session/' in link['href'] and 'showAbstract' not in link['href']]
-        for abstract_link in abstract_links:
-            abstract_url = urljoin(session_url, abstract_link['href'])
-            abstract_response = requests.get(url = abstract_url)
-            abstract_soup = BeautifulSoup(markup = abstract_response.content, features = 'html.parser')
+            abstract_links = [link for link in session_soup.find_all(name = 'a', href = True)
+                              if '/Session/' in link['href'] and 'showAbstract' not in link['href']]
+            for abstract_link in abstract_links:
+                abstract_url = urljoin(session_url, abstract_link['href'])
+                abstract_response = requests.get(url = abstract_url)
+                abstract_soup = BeautifulSoup(markup = abstract_response.content, features = 'html.parser')
 
-            identifier = abstract_link.text.split(':')[0].strip()
-            abstract_data = scrape_abstract(abstract_soup)
-            assert abstract_data.date == session_data.date
-            assert abstract_data.identifier == identifier
-            assert abstract_data.session == session_data.session
+                identifier = abstract_link.text.split(':')[0].strip()
+                abstract_data = scrape_abstract(abstract_soup)
+                assert abstract_data.date == session_data.date
+                assert abstract_data.identifier == identifier
+                assert abstract_data.session == session_data.session
 
 if __name__ == '__main__':
     main()
